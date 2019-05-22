34f1028d2b9bed28b61419bd464d88463f0e4801
rafaelbetta <rafaelbetta@gmail.com>
2019-05-21 21:38:42 -0300

    Tentantiva

--- src/controle/AlunoControler.java
+++ src/controle/AlunoControler.java
@@ -5,22 +5,20 @@ import modelo.Aluno;
 
 public class AlunoControler {
 
-	
 	public void persistir(Aluno aluno) {
 		System.out.println("imprimindo");
 		AlunoRepositorio rep = new AlunoRepositorio();
 		rep.persist(aluno);
 	}
-	
+
 	public Aluno consultar(int id) {
-		
-		
+
 		return null;
-		
+
 	}
-	
+
 	public void remover(int id) {
-		
+
 	}
-	
+
 }
