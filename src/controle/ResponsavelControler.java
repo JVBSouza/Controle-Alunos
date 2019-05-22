10b4880ccecc9a46048edd512bfa4353bb3dfcc2
JBVSouza <joseboing@gmail.com>
2019-05-21 21:29:33 -0300

    Update e delete responsaveis

--- src/controle/ResponsavelControler.java
+++ src/controle/ResponsavelControler.java
@@ -16,4 +16,14 @@ public class ResponsavelControler {
 		rep.find(id);
 	}
 	
+	public void update(int id) {
+		ResponsavelRepositorio rep = new ResponsavelRepositorio();
+		rep.update(id);
+	}
+	
+	public void delete(int id) {
+		ResponsavelRepositorio rep = new ResponsavelRepositorio();
+		rep.delete(id);
+	}
+	
 }
