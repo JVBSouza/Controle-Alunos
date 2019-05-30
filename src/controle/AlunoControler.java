d70df9dce409466d9528107efa7e8d62b3e9ed0c
Rafael Betta <Rafael@Rafaels-MacBook-Pro.local>
2019-05-30 20:12:37 -0300

    Aluno final

--- src/controle/AlunoControler.java
+++ src/controle/AlunoControler.java
@@ -1,24 +1,31 @@
 package controle;
 
 import Repositorio.AlunoRepository;
+import Repositorio.ResponsavelRepository;
 import modelo.Aluno;
 
 public class AlunoControler {
 
 	public void persistir(Aluno aluno) {
 		System.out.println("imprimindo");
+		System.out.println(aluno.toString());
 		AlunoRepository rep = new AlunoRepository();
 		rep.persist(aluno);
 	}
 
-	public Aluno consultar(int id) {
-
-		return null;
-
+	public void buscar(int matricula) {
+		AlunoRepository rep = new AlunoRepository();
+		rep.buscar(matricula);
 	}
-
-	public void remover(int id) {
-
+	
+	public void atualizar(int id) {
+		AlunoRepository rep = new AlunoRepository();
+		rep.atualizar(id);
+	}
+	
+	public void deletar(int id) {
+		AlunoRepository rep = new AlunoRepository();
+		rep.delete(id);
 	}
 
 }
--- src/modelo/Aluno.java
+++ src/modelo/Aluno.java
@@ -1,5 +1,6 @@
 package modelo;
 
+import java.time.LocalDate;
 import java.util.Date;
 
 public class Aluno extends Pessoa {
@@ -8,14 +9,21 @@ public class Aluno extends Pessoa {
 	protected Responsavel responsavel1;
 	protected Responsavel responsavel2;
 	
-	public Aluno(String nome, int cpf, int rg, int telefone, int dataNasc, String endereco) {
-		this.nome = nome;
-		this.cpf = cpf; //cpf tem mais que 11 numeros ver isso
-		this.rg = rg;
-		this.telefone = telefone;
-		this.dataNasc = dataNasc;
-		this.endereco = endereco;
-		this.matricula = 1;
+	public Aluno(String nome, String cpf, int rg, String telefone, LocalDate dataNasc, String endereco) {
+//		this.nome = nome;
+//		this.cpf = cpf; //cpf tem mais que 11 numeros ver isso
+//		this.rg = rg;
+//		this.telefone = telefone;
+//		this.dataNasc = dataNasc;
+//		this.endereco = endereco;
+		this.matricula = 1;  //Why?
+		this.setNome(nome);
+		this.setCpf(cpf);
+		this.setDatanasc(dataNasc);
+		this.setRg(rg);
+		this.setEndereco(endereco);
+		this.setTelefone(telefone);
+		
 	}
 
 	public int getMatricula() {
@@ -28,6 +36,11 @@ public class Aluno extends Pessoa {
 
 	public Responsavel getResponsavel2() {
 		return responsavel2;
+	}
+
+	@Override
+	public String toString() {
+		return "Aluno " + this.getNome()+ ", CPF: "+ this.getCpf();
 	}
 	
 	
